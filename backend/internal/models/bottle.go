93cbe9814acb883bfeb4058c7c4a5e3eb8bb4db6
Zachary Croft <croft.z@northeastern.edu>
2025-09-13 14:21:00 -0400

    optional user id in bottle

--- backend/internal/models/bottle.go
+++ backend/internal/models/bottle.go
@@ -7,7 +7,7 @@ type Bottle struct {
 	Content      *string   `json:"content,omitempty"`
 	Author       *string   `json:"author,omitempty"`
 	TagID        int       `json:"tag_id"`
-	UserID       int       `json:"user_id"`
+	UserID       *int      `json:"user_id,omitempty"`
 	LocationFrom *string   `json:"location_from,omitempty"`
 	CreatedAt    time.Time `json:"created_at"`
 }
